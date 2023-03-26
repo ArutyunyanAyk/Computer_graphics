5e35b17e5a925c70c889df44fb8de4c8df4327c1
Arutyunyan Ayk <90422989+ArutyunyanAyk@users.noreply.github.com>
2023-03-27 00:16:39 +0300

    Add files via upload

---
+++ lab_work1/lab_work1/GrayScaleFilter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.ComponentModel;
+
+
+namespace lab_work1
+{
+    class GrayScaleFilter : Filters
+    {
+        double intensity = 0;
+
+        protected override Color calculateNewPixelColor(Bitmap sourseImage, int x, int y)
+        {
+           
+            Color sourseColor = sourseImage.GetPixel(x, y);
+            intensity = 0.36 * sourseColor.R + 0.53 * sourseColor.G + 0.11 * sourseColor.B;
+            Color resultColor = Color.FromArgb((int)intensity, (int)intensity, (int)intensity);
+
+  
+            return resultColor;
+        }
+    }
+}
